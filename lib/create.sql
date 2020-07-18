769d2b601aeefe4195165f83ba53e48f469c179a
Catherine Price <catprice444@gmail.com>
2020-07-18 15:22:42 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,12 +1,3 @@
--- column	type
--- id	integer
--- name	text
--- age	integer
--- gender	text
--- color	text
--- temperament	text
--- alive	boolean
-
 CREATE TABLE bears 
   (id INTEGER PRIMARY KEY, 
   name TEXT,
