317e8bafe0e8348ee3b751dcf1aa30369f1530d8
Catherine Price <catprice444@gmail.com>
2020-07-18 15:24:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,12 @@
 -- Wendy
 -- unnamed
 
+-- CREATE TABLE bears 
+--   (id INTEGER PRIMARY KEY, 
+--   name TEXT,
+--   age INTEGER,
+--   gender TEXT,
+--   color TEXT,
+--   temperament TEXT,
+--   alive BOOLEAN);
+
